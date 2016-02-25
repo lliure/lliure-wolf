05aba93301716e9dfcfc350b2eef7b0f5d091c54
Jeison Frasson <jomadee@lliure.com.br>
2016-02-25 11:30:34 -0300

    wolf

---
+++ sistema/api/fileup/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+*
+* API Fileup - Plugin CMS
+*
+* @Versão 6.0
+* @Pacote lliure
+* @Entre em contato com o desenvolvedor <lliure@lliure.com.br> http://www.lliure.com.br/
+* @Licença http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+?>
---
+++ sistema/api/navigi/delete.php
@@ -0,0 +1,35 @@
+<?php
+/**
+*
+* Apaga registros
+*
+* @Versão do lliure 8.0
+* @Pacote lliure
+* @Sub-pacote navigi
+*
+* Entre em contato com o desenvolvedor <jomadee@glliure.com.br> http://www.lliure.com.br/
+* Licença http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+header("Content-Type: text/html; charset=ISO-8859-1", true);
+require_once("../../etc/bdconf.php"); 
+require_once("../../includes/jf.funcoes.php"); 
+
+$navigi = unserialize(jf_decode($_SESSION['ll']['user']['token'], $_POST['token']));
+
+if($navigi['delete']){
+	$tabela = $navigi['tabela'];
+	$id = mysql_real_escape_string(substr($_POST['id'], 5));
+
+	jf_delete($tabela, array('id' => $id));
+
+	if(mysql_error() != ''){		
+		if($navigi['debug'])
+			echo mysql_error();
+		else
+			echo 412;
+	}
+} else {
+	echo 403;
+}
+?>
