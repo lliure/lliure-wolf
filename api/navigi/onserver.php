db33edb809eec59ad3de53df8bffc72f5315e81f
mestri.rodrigo@gmail.com <mestri.rodrigo@gmail.com>
2016-11-11 11:14:31 -0200

    - [bug] careção da url de retorno de pesquisa do navigi.

--- api/navigi/onserver.php
+++ api/navigi/onserver.php
@@ -1,16 +1,14 @@
 <?php
 switch(isset($_GET['ac']) ? $_GET['ac'] : '' ){
-case 'pesquisa':	
+	case 'pesquisa':
 
-	$pesquisa = '';
-	if(!empty($_POST['pesquisa']))
-		$pesquisa = '&pesquisa=' . $_POST['pesquisa'];
-	
+		$pesquisa = ((!empty($_POST['pesquisa']))? $pesquisa = '&pesquisa=' . $_POST['pesquisa']: '');
+		header('location: '. $_POST['url']. $pesquisa);
 
-	header('location: index.php'.$_POST['url'].$pesquisa);
 	break;
+	default:
 
-
-default:
+		
+		
 	break;
 }
