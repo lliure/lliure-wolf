ea0a994cadb06819ab832493651573e8d6e621ce
mestri.rodrigo@gmail.com <mestri.rodrigo@gmail.com>
2016-11-13 15:27:21 -0200

    - [new] api Datepiker, para crição de campos de data (que funcionem no firefox).

---
+++ api/datepicker/boot.php
@@ -0,0 +1,10 @@
+<?php
+
+ll::usr('jquery');
+ll::usr('bootstarp');
+
+ll::add(__DIR__. '/css/bootstrap-datepicker3.css', 'css', 2);
+
+ll::add(__DIR__. '/js/bootstrap-datepicker.min.js', 'js', 2);
+ll::add(__DIR__. '/locales/bootstrap-datepicker.pt-BR.min.js', 'js', 2);
+ll::add(__DIR__. '/js/bootstrap-datepicker-default.js', 'js', 2);
---
+++ api/datepicker/js/bootstrap-datepicker-default.js
@@ -0,0 +1,1 @@
+!(function($){$.fn.datepicker.defaults.language='pt-BR';$.fn.datepicker.defaults.templates={leftArrow:'<i class="fa fa-angle-left" aria-hidden="true"></i>',rightArrow:'<i class="fa fa-angle-right" aria-hidden="true"></i>'};})(jQuery);
